55f13bafa941103f8acfb4c979b08894a86e0ad5
HUONG <lamquynhhuong1304@gmail.com>
2019-11-04 17:51:49 +0700

    fix now.js

--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,7 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'index' });
 });
 
-router.get('/index.html', function(req, res, next) {
+router.get('/index', function(req, res, next) {
   res.render('index', { title: 'index' });
 });
 
@@ -15,32 +15,32 @@ router.get('/', function(req, res, next) {
 });
 
 /*Get product */
-router.get('/index-2.html', function(req, res, next) {
+router.get('/index-2', function(req, res, next) {
   res.render('index-2', { title: 'index-2' });
 });
 /*GET filter*/
-router.get('/shop-left-sidebar.html',function(req,res,next){
+router.get('/shop-left-sidebar',function(req,res,next){
   res.render('shop-left-sidebar',{title:'shop-left-sidebar'});
 });
 
 /*Get checkout */
 
-router.get('/checkout.html', function(req, res, next) {
+router.get('/checkout', function(req, res, next) {
   res.render('checkout', { title: 'checkout',  page:'CHECKOUT'});
 });
 
 /* GET login register page */
-router.get('/login-register.html', function(req, res, next) {
+router.get('/login-register', function(req, res, next) {
   res.render('login-register', { title: 'login-register',  page:'LOGIN'});
 });
 
 /* GET cart */
-router.get('/shopping-cart.html', function(req, res, next) {
+router.get('/shopping-cart', function(req, res, next) {
   res.render('shopping-cart', { title: 'shopping-cart',  page:'SHOPPING CART'});
 });
 
 /* GET product detail */
-router.get('/single-product.html', function(req, res, next) {
+router.get('/single-product', function(req, res, next) {
   res.render('single-product', { title: 'single-product',  page:'PRODUCT DETAIL'});
 });
 module.exports = router
